7298ef406cfcf9cf1ab878408f22fb8811b74a9a
xhacker14x <omardanga@yahoo.com>
2023-12-04 16:12:16 +0800

    fixed minor CSS

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -16,7 +16,7 @@ const Layout = () => {
       </div>
 
       <div
-        className={`self-start flex-1 h-screen duration-300 ease-in-out xl:ml-[15%] ${
+        className={`self-start flex-1 h-screen duration-300 ease-in-out ${
           visible ? "ml-0 lg:ml-[20%] xl:ml-[15%]" : "xl:ml-0"
         }`}
       >
